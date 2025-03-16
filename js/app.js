d890e6244029833d91e89602ddf72fe7664fbcab
Mirksn <miroslavbg05@gmail.com>
2025-03-16 22:29:28 +0200

    basicHtml

--- js/app.js
+++
@@ -1,7 +0,0 @@
-var count = document.getElementById("count");
-var button = document.getElementById("Change");
-var currentCount = parseInt(count.textContent);
-button.addEventListener("click", function () {
-  currentCount += 1;
-  count.textContent = currentCount;
-});
