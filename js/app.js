a50ea3b200fcb4b266fea6f30e5404814cd057b2
Mirksn <miroslavbg05@gmail.com>
2025-03-30 22:45:56 +0300

    Added Basic Css For the forms + start of js.

--- js/app.js
+++ js/app.js
@@ -1,2 +1,6 @@
 console.log("app.js loaded at", new Date().toISOString());
 console.log("Page URL:", window.location.href);
+
+//Login Functionality
+const loginBtn = (document.getElementById = "login-btn");
+const registerBtn = document.getElementById;
