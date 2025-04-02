fb43ab760c741ba1b7ae4f029be2cbcb024c22d5
Mirksn <miroslavbg05@gmail.com>
2025-04-02 21:31:38 +0300

    more js

--- firebase.js
+++ firebase.js
@@ -19,3 +19,31 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+const db = getFirestore(app);
+const auth = getAuth(app);
+const storage = getStorage(app);
+
+// Export the Firebase services
+export {
+  db,
+  auth,
+  storage,
+  collection,
+  getDocs,
+  addDoc,
+  deleteDoc,
+  doc,
+  updateDoc,
+  getDoc,
+  setDoc,
+  query,
+  where,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged,
+  ref,
+  uploadBytes,
+  getDownloadURL,
+  deleteObject,
+};
